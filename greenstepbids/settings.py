19e4da7ec89e2f06f972230435b671b42eaa0dce
Diego <diego.boost@gmail.com>
2023-08-28 11:03:17 +0200

    first commit

--- greenstepbids/settings.py
+++ greenstepbids/settings.py
@@ -142,7 +142,11 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'auction', 'static'),
+    os.path.join(BASE_DIR, 'accounts', 'static'),
+    os.path.join(BASE_DIR, 'shoes', 'static'),
+]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
