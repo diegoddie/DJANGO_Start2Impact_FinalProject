7fa64887ad730e08808e3f03be516e2b070358e9
Diego <diego.boost@gmail.com>
2023-08-28 11:42:48 +0200

    first commit

--- greenstepbids/settings.py
+++ greenstepbids/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
